4cbbf24770ab1b4fda02574d79b5a5ba74c91ab4
Samuel <samuelfs96@gmail.com>
2024-02-07 00:25:52 -0400

    feat: create player card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import "./App.css";
+import PlayerCard from "./components/PlayerCard";
+import countryimg from './assets/img/countries/BE.png'
+import playerimg from './assets/img/players/17.png'
 
 function App() {
   return (
     <div>
-      <div className="bg-bg_premier w-screen h-screen bg-center bg-cover contrast-75"></div>
-      <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full">
-        <h1 className="text-3xl font-black text-red-500">Premier League</h1>
+      <div className="bg-bg_premier w-screen h-screen bg-center bg-cover"></div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 w-full h-full flex justify-center items-center gap-8">
+        <PlayerCard playername={{
+          first: 'Kevin',
+          last: 'De Bruyne'
+        }} country={countryimg} img={playerimg}/>
+        <PlayerCard playername={{
+          first: 'Kevin',
+          last: 'De Bruyne'
+        }} country={countryimg} img={playerimg}/>
+        <PlayerCard playername={{
+          first: 'Kevin',
+          last: 'De Bruyne'
+        }} country={countryimg} img={playerimg}/>
       </div>
     </div>
   );
---
+++ src/components/PlayerCard.jsx
@@ -0,0 +1,15 @@
+export default function PlayerCard({
+    playername,
+    country,
+    img
+}) {
+  return (
+    <div className="rounded-lg bg-mci_blue relative w-[100px] h-[140px] shadow-md">
+        <img src={country} alt="country_img" className="absolute left-[-15px] top-[60px] z-10" />
+        <img src={img} alt="player_img" className="absolute top-[-15px]"/>
+        <div className="pb-1 absolute bottom-0 left-0 w-full bg-[#6697c5] rounded-br-lg">
+            <h2 className="text-center text-white font-bold">{playername.last}</h2>
+        </div>
+    </div>
+  )
+}
