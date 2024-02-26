112cb1733e68b1d0e9d1837a1828ef3d7d2ead4a
Samuel <samuelfs96@gmail.com>
2024-02-26 04:39:36 -0400

    feat: add aside structure

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,5 +1,14 @@
 export default function Aside() {
   return (
-    <div className="h-full rounded-tl-[50px] rounded-bl-[50px] opacity-80 bg-mci_blue"></div>
+    <div className="h-full rounded-tl-[50px] rounded-bl-[50px] opacity-80 bg-mci_blue">
+      <div className="p-24">
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+    </div>
   )
 }
