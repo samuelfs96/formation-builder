cd1f3c564013638f2de41a9e7027f04d138b9317
Samuel <samuelfs96@gmail.com>
2024-02-27 00:40:42 -0400

    feat: add players data

---
+++ src/utils/players.js
@@ -0,0 +1,201 @@
+//players
+import StefanOrtega from "../assets/img/players/18.png";
+import EdersonMoraes from "../assets/img/players/31.png";
+import ScottCarson from "../assets/img/players/33.png";
+import KyleWalker from "../assets/img/players/2.png";
+import RubenDias from "../assets/img/players/3.png";
+import JohnStones from "../assets/img/players/5.png";
+import NathanAke from "../assets/img/players/6.png";
+import ManuelAkanji from "../assets/img/players/25.png";
+import RicoLewis from "../assets/img/players/82.png";
+import JoskoGvardiol from "../assets/img/players/24.png";
+import JackGrealish from "../assets/img/players/10.png";
+import Rodri from "../assets/img/players/16.png";
+import KevinBruyne from "../assets/img/players/17.png";
+import BernardoSilva from "../assets/img/players/20.png";
+import SergioGomez from "../assets/img/players/21.png";
+import PhilFoden from "../assets/img/players/47.png";
+import MateoKovacic from "../assets/img/players/8.png";
+import OscarBobb from "../assets/img/players/52.png";
+import MatheusNunes from "../assets/img/players/27.png";
+import ErlingHaaland from "../assets/img/players/9.png";
+import JulianAlvarez from "../assets/img/players/19.png";
+import JeremyDoku from "../assets/img/players/11.png";
+
+//countries
+import BE from "../assets/img/countries/BE.png";
+import GER from "../assets/img/countries/GER.png";
+import BRA from "../assets/img/countries/BRA.png";
+import ENG from "../assets/img/countries/ENG.png";
+import POR from "../assets/img/countries/POR.png";
+import NET from "../assets/img/countries/NET.png";
+import SWI from "../assets/img/countries/SWI.png";
+import CRO from "../assets/img/countries/CRO.png";
+import ESP from "../assets/img/countries/ESP.png";
+import NOR from "../assets/img/countries/NOR.png";
+import ARG from "../assets/img/countries/ARG.png";
+
+export const players = {
+  Goalkeepers: [
+    {
+      number: 18,
+      firstName: "Stefan",
+      lastName: "Ortega",
+      country: GER,
+      img: StefanOrtega,
+    },
+    {
+      number: 31,
+      firstName: "Ederson",
+      lastName: "Moraes",
+      country: BRA,
+      img: EdersonMoraes,
+    },
+    {
+      number: 33,
+      firstName: "Scott",
+      lastName: "Carson",
+      country: ENG,
+      img: ScottCarson,
+    },
+  ],
+  Defenders: [
+    {
+      number: 2,
+      firstName: "Kyle",
+      lastName: "Walker",
+      country: ENG,
+      img: KyleWalker,
+    },
+    {
+      number: 3,
+      firstName: "Rúben",
+      lastName: "Dias",
+      country: POR,
+      img: RubenDias,
+    },
+    {
+      number: 5,
+      firstName: "John",
+      lastName: "Stones",
+      country: ENG,
+      img: JohnStones,
+    },
+    {
+      number: 6,
+      firstName: "Nathan",
+      lastName: "Aké",
+      country: NET,
+      img: NathanAke,
+    },
+    {
+      number: 25,
+      firstName: "Manuel",
+      lastName: "Akanji",
+      country: SWI,
+      img: ManuelAkanji,
+    },
+    {
+      number: 82,
+      firstName: "Rico",
+      lastName: "Lewis",
+      country: ENG,
+      img: RicoLewis,
+    },
+    {
+      number: 24,
+      firstName: "Josko",
+      lastName: "Gvardiol",
+      country: CRO,
+      img: JoskoGvardiol,
+    },
+  ],
+  Midfielders: [
+    {
+      number: 10,
+      firstName: "Jack",
+      lastName: "Grealish",
+      country: ENG,
+      img: JackGrealish,
+    },
+    {
+      number: 16,
+      firstName: "Rodri",
+      lastName: "",
+      country: ESP,
+      img: Rodri,
+    },
+    {
+      number: 17,
+      firstName: "Kevin",
+      lastName: "De Bruyne",
+      country: BE,
+      img: KevinBruyne,
+    },
+    {
+      number: 20,
+      firstName: "Bernardo",
+      lastName: "Silva",
+      country: POR,
+      img: BernardoSilva,
+    },
+    {
+      number: 21,
+      firstName: "Sergio",
+      lastName: "Gómez",
+      country: ESP,
+      img: SergioGomez,
+    },
+    {
+      number: 47,
+      firstName: "Phil",
+      lastName: "Foden",
+      country: ENG,
+      img: PhilFoden,
+    },
+    {
+      number: 8,
+      firstName: "Mateo",
+      lastName: "Kovacic",
+      country: CRO,
+      img: MateoKovacic,
+    },
+    {
+      number: 52,
+      firstName: "Oscar",
+      lastName: "Bobb",
+      country: NOR,
+      img: OscarBobb,
+    },
+    {
+      number: 27,
+      firstName: "Matheus",
+      lastName: "Nunes",
+      country: POR,
+      img: MatheusNunes,
+    },
+  ],
+  Forwards: [
+    {
+      number: 9,
+      firstName: "Erling",
+      lastName: "Haaland",
+      country: NOR,
+      img: ErlingHaaland,
+    },
+    {
+      number: 19,
+      firstName: "Julián",
+      lastName: "Álvarez",
+      country: ARG,
+      img: JulianAlvarez,
+    },
+    {
+      number: 11,
+      firstName: "Jérémy",
+      lastName: "Doku",
+      country: BE,
+      img: JeremyDoku,
+    },
+  ],
+};
